6d800667d1ecc3ff00129bb6787287e98899e0d9
带脑子的草履虫虫 <2485706183@qq.com>
2022-12-04 17:12:28 +0800

    评论，二级评论，回复接口开发完成

--- routes/interact.js
+++ routes/interact.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { postCollection, postLike, getCheckIsLike, getCheckIsCollect, delCollection, delLike } from '../routesHandler/interact.js'
+import { postDelComment, postDelReply, postCollection, postLike, getCheckIsLike, getCheckIsCollect, delCollection, delLike, postAddComment, getComments, getCommentReply, getComment, postAddReply } from '../routesHandler/interact.js'
 const router = Router()
 
 // 添加收藏
@@ -10,5 +10,18 @@ router.get('/iscollect/:id', getCheckIsCollect)
 // 取消点赞收藏
 router.post('/dislike/:id', delLike)
 router.post('/discollect/:id', delCollection)
+// 添加评论
+router.post('/addcomment', postAddComment)
+// 拉取评论
+router.get('/getcomments', getComments)
+router.get('/getcomment', getComment)
+// 拉取二级评论
+router.get('/getcommentreply', getCommentReply)
+// 添加二级评论或回复
+router.post('/postaddreply', postAddReply)
+// 删除评论
+router.post('/postdelreply', postDelReply)
+router.post('/postdelcomment', postDelComment)
+
 
 export default router
--- routesHandler/interact.js
+++ routesHandler/interact.js
@@ -127,7 +127,6 @@ export const delCollection = function (req, res) {
     })
   })
 }
-
 export const delLike = function (req, res) {
   const token = req.cookies.access_token
   if (!token) return res.status(401).send('请登录后进行操作 !')
@@ -150,6 +149,159 @@ export const delLike = function (req, res) {
       // 没有关联关系则返回警告
       else {
         return res.status(403).send('已取消点赞, 请勿重复操作')
+      }
+    })
+  })
+}
+
+// 添加评论
+export const postAddComment = function (req, res) {
+  const token = req.cookies.access_token
+  if (!token) return res.status(401).send('请登录后进行操作 !')
+  const { postId, formdata } = req.body
+  const { commentText, date } = formdata
+  // 鉴权
+  jwt.verify(token, 'privateKey', function (err, tokenId) {
+    if (err) return res.status(401).send('非法token值')
+    let addCommentStr = 'INSERT INTO comments(commentuserid,commentpostid,commenttext,commentdate) VALUE(?,?,?,?)'
+    db.query(addCommentStr, [tokenId.id, postId, commentText, date], (err) => {
+      if (err) return res.status(500).send(err)
+      res.status(200).send({ message: '发布评论成功' })
+    })
+  })
+}
+
+//拉取评论, 获取评论的同时将首个回复一并进行传递
+export const getComments = function (req, res) {
+  const { postid, page, pagesize } = req.query
+  let getLengthStr = 'SELECT count(*) length FROM comments WHERE commentpostid = ? AND commentstatus = "1"'
+  db.query(getLengthStr, postid, (err, result) => {
+    if (err) return res.status(500).send(err)
+    let getCommentsStr = 'SELECT comments.id,`commenttext`,`username`,`img`,`commentdate` FROM comments JOIN users ON users.id = comments.commentuserid WHERE commentpostid = ? AND commentstatus = "1"  ORDER BY comments.id DESC LIMIT ?,?'
+    db.query(getCommentsStr, [postid, ((+page) * (+pagesize)), +pagesize], (err, data) => {
+      if (err) return res.status(500).send(err)
+      const commentIds = data.map(item => {
+        const { id } = item
+        return id
+      })
+      // 有评论再进行回复筛选
+      if (commentIds.length === 0) {
+        res.status(200).send({ comments: data, length: result[0].length })
+      } else {
+        let getReplyStr = 'SELECT reply.id AS replyId,publisher.id AS publisherId,publisher.username AS publisherUsername,subscripter.id AS subscripterId,subscripter.username AS subscripterUsername,`replytext`,`replydate`,`isreply`, `commentid` FROM comment_reply AS reply JOIN users AS publisher ON reply.publisherid = publisher.id JOIN users AS subscripter ON reply.subscripterid = subscripter.id WHERE reply.commentid in(?) AND replystatus = "1" ORDER BY reply.id DESC '
+        db.query(getReplyStr, [commentIds], (err, replys) => {
+          if (err) return res.status(500).send(err)
+          // 过滤筛选第一条回复评论
+          let filterHelper = []
+          const newReplys = replys.filter(item => {
+            if (filterHelper.indexOf(item.commentid) === -1) {
+              filterHelper.push(item.commentid)
+              return true
+            } else {
+              return false
+            }
+          })
+          // 拼接首个评论以及回复
+          const newData = data.map(each => {
+            const firstReply = newReplys.find(item => {
+              return item.commentid === each.id
+            })
+            each.reply = firstReply
+            return each
+          })
+          res.status(200).send({ comments: newData, length: result[0].length })
+        })
+      }
+    })
+  })
+}
+// 获取指定评论
+export const getComment = function (req, res) {
+  const { commentid } = req.query
+  let getCommentsStr = 'SELECT users.id AS userid,comments.id,`commenttext`,`username`,`img`,`commentdate`,`commentpostid` FROM users JOIN comments ON users.id = comments.commentuserid WHERE comments.id = ? AND commentstatus = "1"'
+  db.query(getCommentsStr, commentid, (err, data) => {
+    if (err) return res.status(500).send(err)
+    res.status(200).send(data[0])
+  })
+
+}
+
+// 获取二级评论回复
+export const getCommentReply = function (req, res) {
+  const { commentid, page, pagesize } = req.query
+  let getLengthStr = 'SELECT count(*) length FROM comment_reply WHERE commentid = ? AND replystatus = "1"'
+  db.query(getLengthStr, commentid, (err, result) => {
+    if (err) return res.status(500).send(err)
+    // 多表联查 + 键自联 实现多对多和一表多查询
+    let getReplyStr = 'SELECT reply.id AS replyId,publisher.id AS publisherId,publisher.username AS publisherUsername,subscripter.id AS subscripterId,subscripter.username AS subscripterUsername,`replytext`,`replydate`,`isreply` FROM comment_reply AS reply JOIN users AS publisher ON reply.publisherid = publisher.id JOIN users AS subscripter ON reply.subscripterid = subscripter.id WHERE reply.commentid = ? AND replystatus = "1" ORDER BY reply.id DESC LIMIT ?,? '
+    db.query(getReplyStr, [commentid, ((+page) * (+pagesize)), +pagesize], (err, data) => {
+      if (err) return res.status(500).send(err)
+      res.status(200).send({ replyInfo: data, replyLength: result[0].length })
+    })
+  })
+}
+// 添加二级评论或回复
+export const postAddReply = function (req, res) {
+  const token = req.cookies.access_token
+  if (!token) return res.status(401).send('请登录后进行操作 !')
+  const { commentText, date, subscripterId, commentId, isReply } = req.body
+  jwt.verify(token, 'privateKey', function (err, tokenId) {
+    if (err) return res.status(401).send('非法token值')
+    let addReplyStr = 'INSERT INTO comment_reply(commentid,publisherid,replytext,subscripterid,replydate,isreply) VALUE(?,?,?,?,?,?)'
+    db.query(addReplyStr, [commentId, tokenId.id, commentText, subscripterId, date, isReply], (err) => {
+      if (err) return res.status(500).send(err)
+      res.status(200).send({ message: '发布评论成功' })
+    })
+  })
+}
+
+// 删除评论
+export const postDelComment = function (req, res) {
+  const token = req.cookies.access_token
+  if (!token) return res.status(401).send('请登录后进行操作 !')
+  const { commentId } = req.body
+  jwt.verify(token, 'privateKey', function (err, tokenId) {
+    if (err) return res.status(401).send('非法token值')
+    let checkHostStr = 'SELECT `uid` AS postuserid,`commentuserid` FROM comments JOIN posts ON commentpostid = posts.id WHERE comments.id = ?'
+    db.query(checkHostStr, [commentId], (err, result) => {
+      if (err) return res.status(500).send(err)
+      // 有删除权限的id，文章作者或评论发起人
+      if (result[0].postuserid == tokenId.id || result[0].commentuserid == tokenId.id) {
+        let delCommentStr = 'UPDATE comments SET commentstatus = ? WHERE id = ?'
+        db.query(delCommentStr, [0, commentId], (err) => {
+          if (err) return res.status(500).send(err)
+          let delReplyStr = 'UPDATE comment_reply SET replystatus = ?  WHERE commentid = ? '
+          db.query(delReplyStr, [0, commentId], (err) => {
+            if (err) return res.status(500).send(err)
+            res.status(200).send({ message: '已删除评论 !' })
+          })
+        })
+      } else {
+        res.status(403).send('无操作权限 !')
+      }
+    })
+  })
+}
+// 删除回复
+export const postDelReply = function (req, res) {
+  const token = req.cookies.access_token
+  if (!token) return res.status(401).send('请登录后进行操作 !')
+  const { replyId } = req.body
+  // 文章发布者或者评论回复者才能删除回复
+  jwt.verify(token, 'privateKey', function (err, tokenId) {
+    if (err) return res.status(401).send('非法token值')
+    let checkHostStr = 'SELECT `uid` AS postuserid,`publisherid`,comment_reply.id AS replyId FROM comments JOIN posts ON commentpostid = posts.id JOIN comment_reply ON commentid = comments.id WHERE comment_reply.id = ?'
+    db.query(checkHostStr, [replyId], (err, result) => {
+      if (err) return res.status(500).send(err)
+      // 有删除权限的id，文章作者或评论发起人
+      if (result[0].postuserid == tokenId.id || result[0].publisherid == tokenId.id) {
+        let delReplyStr = 'UPDATE comment_reply SET replystatus = ? WHERE id = ?'
+        db.query(delReplyStr, [0, replyId], (err) => {
+          if (err) return res.status(500).send(err)
+          res.status(200).send({ message: '已删除评论 !' })
+        })
+      } else {
+        res.status(403).send('无操作权限 !')
       }
     })
   })
